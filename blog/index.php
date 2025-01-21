2a64d1e83e64e17f476d8dcba3a7f14caa0df9ba
dev <azertwxcvbn259@gmail.com>
2025-01-21 14:59:27 +0100

    séparation des données avec une structure MVC

--- blog/index.php
+++ blog/index.php
@@ -1,30 +1,8 @@
 <?php
-// Connexion à la base de données
-try {
-   $database = new PDO('mysql:host=localhost;dbname=MVC;charset=utf8', 'root', 'Fc.barcelone10');
-} catch (Exception $e) {
-   die('Erreur : ' . $e->getMessage());
-}
 
-// On récupère les 5 derniers billets
-$statement = $database->query("SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5");
+require('src/model.php');
 
-if (!$statement) {
-   die('Erreur de requête SQL : ' . print_r($database->errorInfo(), true));
-}
-
-$posts = [];
-
-while ($row = $statement->fetch()) {
-   $post = [
-      'title' => $row['titre'],
-      'content' => $row['contenu'],
-      'frenchCreationDate' => $row['date_creation_fr'],
-   ];
-
-   $posts[] = $post;
-}
+$posts = getPosts();
 
 require('templates/homepage.php');
-
 ?>
